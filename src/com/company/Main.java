684876aed92b33f663431efdcbebaa2ef40b87b4
kiparisa91 <kiparisa91@gmail.com>
2022-04-19 18:55:29 +0500

    Дз

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,30 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        // Задача1
+        int i = 1;
+        while (i <= 10) {
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.print("\n");
+
+        for (int j=10 ; j >= 1; j--) {
+            System.out.print(j + " ");
+        }
+        System.out.print("\n");
+
+        //Задача2
+        int friday = 1;
+        for (; friday <=31; friday=friday+7) {
+            System.out.println("Сегодня пятница " + friday + "-е число. Необходимо подготовить отчет.");
+        }
+
+        //Задача3
+        for(int year=1822; year >= 1822 && year <= 2122; year++) {
+            if (year % 79 == 0)
+                System.out.println(year);
+        }
+
     }
 }
